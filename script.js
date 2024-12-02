d1fe71a0054663b0528327d2b3641fe880a55f5c
zurisanaa <zsnorman@cps.edu>
2024-12-02 17:21:22 +0000

    k

---
+++ script.js
@@ -0,0 +1,8 @@
+let left = document.querySelector('.left');
+let right = document.querySelector('.right');
+let text = document.querySelector('p');
+
+left.addEventListener('click', function()){
+    console.log ("left was clicked");
+}
+
